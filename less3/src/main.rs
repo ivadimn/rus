e3ecd49e192b4ceba9f20cfb77584b7d8970b5b6
vadim <ivadimn@mail.ru>
2026-03-06 13:57:12 +0300

    simple types

--- less3/src/main.rs
+++ less3/src/main.rs
@@ -8,4 +8,23 @@ fn main() {
     println!("{y}");
     println!("{}", f64::EPSILON);
     println!("{}", 12.2 % 5.5);
+    
+
+    let l = "абвгдж".len();
+    println!("{}", l);
+    let c = "абвгдж".chars().count();
+    println!("{}", c);
+    let s = &("абвгдж")[4..];
+    println!("{}", s);
+    let f = "абвгдж".starts_with("абв");
+    println!("{}", f);
+    let f = "абвгдж".ends_with("гдж");
+    println!("{}", f);
+    let _o = "абвгдж".find("вг");
+    //println!("{}", Some(o));
+    let s = "абвгдж".replace("абв", "АБВ");
+    println!("{}", s);
+    //let s = "абвгдж".split(" ");
+    let s = "  абвгдж   ".trim();
+    println!("{}", s);
 }
