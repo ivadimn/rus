f5e8e05bb6fe8e9ab52f84895d2528c72ff66e97
vadim <ivadimn@mail.ru>
2026-03-16 16:47:24 +0300

    update

--- vars/src/main.rs
+++ vars/src/main.rs
@@ -16,4 +16,8 @@ fn main() {
     let foo = f();
     println!("{}, {}", foo.val, foo.flag);
 
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    println!("{five_hundred}");
+
 }
