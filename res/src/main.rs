c9ac538c41c6e20d363f5a823a5e6f5aba90e5a2
vadim <ivadimn>
2026-04-19 18:00:29 +0300

    add resource

---
+++ res/src/main.rs
@@ -0,0 +1,16 @@
+
+
+fn foo(s: &str) {
+    println!("{s}");
+}
+
+fn calculate_length(s: &String) ->usize {
+    s.len()
+}
+
+fn main() {
+    let s1 = String::from("Hello");
+    let len = calculate_length(&s1);
+
+    println!("The length of {} is {}", s1, len);
+}
