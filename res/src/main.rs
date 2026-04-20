bb6ba16e7a0014b283d71cba6d63eda1cb434f59
vadim <ivadimn@mail.ru>
2026-04-20 17:46:41 +0300

    update res

--- res/src/main.rs
+++ res/src/main.rs
@@ -1,16 +1,39 @@
 
 
-fn foo(s: &str) {
-    println!("{s}");
+struct Account {
+    balance: i64,
 }
 
-fn calculate_length(s: &String) ->usize {
-    s.len()
+impl Drop for Account {
+    fn drop(&mut self) {
+        if self.balance != 0 {
+            println!("Предупреждение: Баланс не равен нулю! {}", self.balance);
+        } else {
+            println!("Счёт успешно удалён!");
+        }
+    }
+}
+
+fn print_balance(a: Account) {
+    println!("Текущий баланнс счёта: {}", a.balance);
+}
+
+fn transfer_balance(a1: &mut Account, a2: &mut Account, summa: i64) {
+    a1.balance -= summa;
+    a2.balance += summa;
+
+}
+
+fn destroy_account(mut d: Account, a: &mut Account) {
+    let summa = d.balance;
+    transfer_balance(&mut d, a, summa);
 }
 
 fn main() {
-    let s1 = String::from("Hello");
-    let len = calculate_length(&s1);
-
-    println!("The length of {} is {}", s1, len);
+    let a1 = Account {balance: 200};
+    let mut a2 = Account {balance: 0};
+    //let a3 = Account {balance: 40000};
+    //let a1 = Account {balance: 0};
+    destroy_account(a1,  &mut a2);
+    print_balance(a2);
 }
