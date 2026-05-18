fe703f90cfc304f21b3679940cee28d8547becc8
Vadim <ivadimn@mail.ru>
2026-05-18 20:24:45 +0300

    update add references

---
+++ pointers/src/main.rs
@@ -0,0 +1,108 @@
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{some_string}");
+} 
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{some_integer}");
+} 
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+    s
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+struct Account {
+    name: String,
+    balance: i64
+}
+
+impl Drop for Account {
+    fn drop(&mut self) {
+        if self.balance != 0 {
+            println!("Предупреждение: баланс не равен 0!!");
+        }
+        else {
+            println!("Счёт безопасно удалён.");
+        }
+    }
+}
+
+fn print_balance(a: &Account) {
+    println!("Текущий баланс счёта {}: {}",a.name, a.balance);
+}
+
+fn transfer_funds(a_to: &mut Account, a_from: &mut Account, summa: i64) {
+    a_to.balance += summa;
+    a_from.balance -= summa;
+}
+
+fn destroy_account(mut a_del: Account, a_to: &mut Account) {
+    a_to.balance += a_del.balance;
+    a_del.balance = 0;
+}
+
+struct Bank {
+    accounts: Vec<Account>,
+    credit_rate: u32,
+    debit_reate: u32
+}
+
+fn account_interest(bank: &mut Bank) {
+    for account in bank.accounts.iter_mut() {
+        account.balance += account.balance / 100;
+    }
+}
+
+fn merge_banks(bank_from: Bank, bank_to: &mut Bank) {
+    bank_to.credit_rate = bank_from.credit_rate;
+    bank_to.debit_reate = bank_from.debit_reate;
+    for account in bank_from.accounts {
+        bank_to.accounts.push(account)
+    }
+    
+}
+
+fn bank_info(bank: &Bank) {
+    println!("Дебит:  {}", bank.debit_reate);
+    println!("Кредит: {}", bank.credit_rate);
+    for account in bank.accounts.iter() {
+        print_balance(account);
+    }
+}
+
+fn main() { 
+    let mut s1 = Account {name: String::from("Счёт 1"), balance: 250};
+    let mut s2 = Account {name: String::from("Счёт 2"), balance: 150};
+    let mut s3 = Account {name: String::from("Счёт 3"), balance: 0};
+    
+    transfer_funds(&mut s3, &mut s1, 50);
+    destroy_account(s2, &mut s3);
+    //print_balance(&s3);
+    let mut bank = Bank {accounts: Vec::new(), credit_rate: 0, debit_reate: 0};
+    let mut bank2 = Bank {accounts: Vec::new(), credit_rate: 0, debit_reate: 0};
+
+    bank.accounts.push(s1);
+    //bank.accounts.push(s2);
+    bank.accounts.push(s3);
+    bank_info(&bank);
+    merge_banks(bank, &mut bank2);
+    bank_info(&bank2);
+
+
+}
--- res/src/main.rs
+++ res/src/main.rs
@@ -1,12 +1,12 @@
 
 
 struct Account {
-    balance: i64,
+    balance: f64,
 }
 
 impl Drop for Account {
     fn drop(&mut self) {
-        if self.balance != 0 {
+        if self.balance != 0.0 {
             println!("Предупреждение: Баланс не равен нулю! {}", self.balance);
         } else {
             println!("Счёт успешно удалён!");
@@ -14,11 +14,31 @@ impl Drop for Account {
     }
 }
 
-fn print_balance(a: Account) {
+
+struct Bank {
+    accounts: Vec<Account>,
+    credit_rate: u32,
+    debit_rate: u32 
+}
+
+
+fn accrue_interest(bank: &mut Bank) {
+    for account in bank.accounts.iter_mut() {
+        account.balance += account.balance * 0.01;
+    }
+}
+
+fn print_bank_balance(bank: &Bank) {
+    for account in bank.accounts.iter() {
+        print_balance(account);
+    }
+}
+
+fn print_balance(a: &Account) {
     println!("Текущий баланнс счёта: {}", a.balance);
 }
 
-fn transfer_balance(a1: &mut Account, a2: &mut Account, summa: i64) {
+fn transfer_balance(a1: &mut Account, a2: &mut Account, summa: f64) {
     a1.balance -= summa;
     a2.balance += summa;
 
@@ -41,12 +61,29 @@ fn takes_and_gives_back(a_string: String) -> String {
 fn main() {
     //let s1 = gives_ownership();
 
-    let mut s = String::from("hello");
+    // let mut s = String::from("hello");
 
-    s.push_str(", world!");
-    println!("{s}");
-    let s1 = s.clone();
-    println!("{s1}");
-    println!("{s}");
+    // s.push_str(", world!");
+    // println!("{s}");
+    // let s1 = s.clone();
+    // println!("{s1}");
+    // println!("{s}");
+
+    let mut a1 = Account{balance: 250.0};
+    let a2 = Account{balance: 100.0};
+    let mut a3 = Account{balance: 280.0};
+
+    print_balance(&a2);
+    //transfer_balance(&mut a1, &mut a3, 250.0);
+    
+
+    let mut bank = Bank {accounts: Vec::new(), credit_rate: 0, debit_rate: 0};
+    bank.accounts.push(a1);
+    bank.accounts.push(a2);
+    bank.accounts.push(a3);
+
+    accrue_interest(&mut bank);
+    print_bank_balance(&bank);
+
     
 }
