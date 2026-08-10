5d5ba592a298a7cef4ca470c90213fa6f5ba6c32
ivadimn <ivadimn@mail.ru>
2026-08-10 17:38:07 +0300

    update files

--- files/src/main.rs
+++ files/src/main.rs
@@ -2,6 +2,7 @@
 //use std::sync::{Arc, Mutex};
 //use std::thread;
 use std::fmt::{self, write};
+use std::mem;
 
 fn longest_common_prefix<'a>(x: &'a str, y: &'a str) -> &'a str{
     let  min_length = std::cmp::min(x.len(), y.len());
@@ -28,20 +29,45 @@ fn transponse(matrix: Matrix) -> Matrix {
     Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
 }
 
+fn apply<F>(f: F) where 
+    F: FnOnce() {
+
+    f();
+}
+
+fn apply_to_3<F>(f: F) -> i32 where 
+    F: Fn(i32) -> i32 {
+        f(3)
+    }
 
 #[allow(unreachable_code)]
 fn main() {
-    fn function(i: i32) -> i32 {i + 1}
+    let greeting = "привет";
 
-    let closure_annotated = |i: i32| -> i32 {i + 1};
-    let closure_inferred = |i  | i + 1;
+    let mut farewell = "пока".to_owned();
 
-    let i = 1;
-    println!("Функция: {}", function(i));
-    println!("Замыкание с указанием типа: {}", closure_annotated(i));
-    println!("Замыкание с выводом типа: {}", closure_inferred(i));
+    // Захват двух переменных: `greeting` по ссылке и
+    // `farewell` по значению.
+    let diary = || {
+        // `greeting` захватывается по ссылке: требует `Fn`.
+        println!("Я сказал {}.", greeting);
 
-    let one = || 1;
-    println!("Замыкание возвращающее один {}", one());
+        // Изменяемость требует от `farewell` быть захваченным
+        // по изменяемой ссылке. Сейчас требуется `FnMut`.
+        farewell.push_str("!!!");
+        println!("Потом я закричал {}.", farewell);
+        println!("Теперь я могу поспать. zzzzz");
+
+        // Ручной вызов удаления требуется от `farewell`
+        // быть захваченным по значению. Теперь требуется `FnOnce`.
+        mem::drop(farewell);
+    };
+
+    apply(diary);
+
+    let double = |x| x * 2;
+
+    println!("Удвоенное 3: {}", apply_to_3(double));
+
 
 }
