3d1d809a8eeb26e34cd4fe3f7d6517e2538a1c95
ivadimn <ivadimn@mail.ru>
2026-08-24 17:38:19 +0300

    update func

--- ptrs/src/main.rs
+++ ptrs/src/main.rs
@@ -1,9 +1,21 @@
-use std::rc::Rc;
+use std::{rc::Rc, sync::Arc};
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn apply_operation(x: i32, y: i32, operation: fn(i32, i32) -> i32) -> i32 {
+    operation(x, y)
+}
 
 fn main() {
-    let data = Rc::new(42);
-    let clone1 = Rc::clone(&data);
-    let clone2 = Rc::clone(&data);
+    let data = Arc::new(42);
+    let clone1 = Arc::clone(&data);
+    let clone2 = Arc::clone(&data);
 
-    println!("Reference count: {}", Rc::strong_count(&data));
+    println!("Reference count: {}", Arc::strong_count(&data));
+
+    let sum: fn(i32, i32) -> i32 = add;
+    println!("Summa: {}", sum(4, 8));
+    println!("Apply operation: {}", apply_operation(3, 8, add));
 }
