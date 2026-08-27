a089a26993aadcf0d5fd192637130e297437d1a8
ivadimn <ivadimn@mail.ru>
2026-08-27 17:45:01 +0300

    update files

--- files/src/main.rs
+++ files/src/main.rs
@@ -1,48 +1,65 @@
-use std::fs::{self, Metadata};
-use std::io::{self, Write};
-use std::path::Path;
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, Read, Write};
+
+struct Person {
+    name: [u8; 512],
+    age: u8,
+}
+
+impl Person {
+    pub fn new(name: String, age: u8) -> Self {
+        let mut obj = Self {
+            name: [0; 512],
+            age 
+        };
+        
+        let tmp =  name.as_bytes();
+        for (i, b) in tmp.into_iter().enumerate() {
+            obj.name[i] = *b;    
+        }
+        obj
+    }
+
+    pub fn get_name(&self) -> String {
+        let vec_name = Vec::from_iter(self.name.into_iter());
+        String::from_utf8(vec_name).unwrap()
+    }
+
+    pub fn save(file_name: &str, data: Vec<&Self>) -> io::Result<()> {
+        let mut file: File; 
+        let file_result = OpenOptions::new()
+            .write(true)  // Разрешаем запись
+            .append(true) // Устанавливаем режим дозаписи (append)
+            .create(true) // Создаем файл, если он не существует
+            .open(file_name); // Открываем файл
+
+        match file_result {
+            Ok(f) => file = f,
+            Err(msg) => {
+                eprintln!("Ошибка при открытии файла: {}", msg);
+                return Err(msg);
+            }
+        }
+        for p in data {
+            file.write(&p.name)?;
+            file.write(&[p.age])?;
+        }
+        Ok(())
+    } 
+}
 
 fn main() -> std::io::Result<()>  {
-    // let content = fs::read_to_string("http.txt");
-    // match content {
-    //     Ok(s) => println!("Содержимое файла: \n{}", s),
-    //     Err(err) => println!("Ошибка чтения файла: {}", err),
-    // }
-    // let bytes = fs::read("http.txt")?;
-    // println!("Прочитано {} байт", bytes.len());
+    
+    //let person = Person {name: }
+    let person = Person::new(String::from("Пупкин Иван Семенович"), 77);
 
-    // fs::write("output.txt", "Привет мир....")?;
-
-    // let mut file = fs::OpenOptions::new()
-    //     .append(true)
-    //     .create(true)
-    //     .open("log.txt")?;
-    // writeln!(file, "Новая запись в логе")?;
-
-    //fs::create_dir("new_folder")?;
-
-    //fs::create_dir_all("path/to/nested/folder")?;
-
-    let entries = fs::read_dir(".")?;
-    for entry in entries {
-        let entry = entry?;
-        let path = entry.path();
-        let metadata = entry.metadata()?;
-        let file_type = if metadata.is_dir() {
-            "Директория"
-        } else if metadata.is_file() {
-            "Файл"
-        } else {
-            "Другое"
-        };
-        println!("{}: {}", path.display(), file_type);
+    let vec_per:  Vec<&Person> = vec![&person];
+    
+    let result = Person::save("person.data", vec_per);
+    match result {
+        Ok(_) => println!("Данные успешно записаны"),
+        Err(msg) => println!("Ошибка: {}", msg),
     }
-
-    if let Err(e) = process_file("http.txt") {
-        eprintln!("Ошибка: {}", e)
-    }
-   
-
     Ok(())
     
 }
