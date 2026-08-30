593e7dbd267e7ae37106223e308b6b8b87b21fe3
Vadim <ivadimn@mail.ru>
2026-08-30 20:14:06 +0300

    update common

--- common/src/main.rs
+++ common/src/main.rs
@@ -1,6 +1,10 @@
 use std::ops::Add;
+use std::io::BufRead;
 mod jsn;
 use jsn::{Person, struct_to_bytes};
+mod serr;
+use serr::MyError;
+
 
 struct Complex {
     real: f64,
@@ -17,10 +21,28 @@ impl Add for Complex {
             img: self.img + rhs.img,
         }
     }
-
-
 }
 
+fn find_user(username: &str) -> Result<u32, String> {
+
+    let f = std::fs::File::open("/etc/passwd")
+        .map_err(|e| format!("Faild to open password file: {:?}", e))?;
+    Ok(45)
+}
+
+const MAX_LEN: usize = 1024;
+
+fn first_line(filename: &str) -> Result<String, MyError> {
+    let file = std::fs::File::open(filename)?;
+    let mut reader = std::io::BufReader::new(file);
+    let mut buf = vec![];
+    let len = reader.read_until(b'\n', &mut buf)?;
+    let result = String::from_utf8(buf).map_err(MyError::Utf8)?;
+    if result.len() > MAX_LEN {
+        return Err(MyError::General(format!("Line too long: {}", len)));
+    }
+    Ok(result)
+}
 
 fn main() {
 
@@ -42,4 +64,15 @@ fn main() {
     let c2 = Complex {real: 1.26, img: 0.12};
     let c3 = c1 + c2;
     println!("Complex {} + {}i", c3.real, c3.img);
+
+    if let Ok(id) = find_user("vadim") {
+        println!("User Id: {}", id);
+    }
+    
+    if let Ok(line) = first_line("output.txt") {
+        println!("{}", line);
+    }
+    else {
+        println!("Error!");
+    }
 }
