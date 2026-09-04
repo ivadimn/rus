953e026ee691c06c99ea3de7ec1c9ff0637a311b
ivadimn <ivadimn@mail.ru>
2026-09-04 13:33:30 +0300

    update arr

--- arr/src/main.rs
+++ arr/src/main.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 fn main() {
     let tic_tac_toe: [[char; 3]; 3] = [
         [' ', 'X', '0'],
@@ -7,6 +9,26 @@ fn main() {
     
     println!("Первоначальное значение массива");
     print_tic_tac_toe(&tic_tac_toe);
+
+    let fruits = vec!["apple", "banan", "abricos", "orange"];
+    let second = fruits.get(5);
+    match second {
+        Some(fruit) => println!("Second element: {}", fruit),
+        None => println!("Nothing..."),
+    }
+
+    let mut numbers = vec![11, 12, 13, 14, 15, 16];
+    for number in numbers.iter_mut() {
+        *number *= 2;
+    }
+    println!("numbers: {:?}", numbers);
+
+    let person = ("Alice", 25, true);
+    match person {
+        (name, age, true) => println!("{}: возраст {} лет, работает", name, age),
+        (name, age, false) => println!("{}: возраст {} лет, не работает", name, age),
+        _ => println!("Unknown..."),
+    }
 
 
 }
